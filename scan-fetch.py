8a062c1b451a9d2cf7e77e0e3c3afdc4f875b119
stefan <s-koch@gmx.net>
2015-09-19 16:46:33 +0200

    added license and improved wording

--- scan-fetch.py
+++ scan-fetch.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+########################################################################################
+#
+# Copyright by Stefan Koch <StefanKoch@gmx.org>, 2015
+#
+# This file is part of HP Office Jet 6500A scan fetcher (hpojet-fetcher)
+#
+#    hpojet-fetcher is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    hpojet-fetcher is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+########################################################################################
 
 ##################################################################################
 # Imports
@@ -45,7 +65,7 @@ def easyCopy(fileList, onlyFirst):
 		try:
 			os.remove(src)
 		except:
-			print("file still in use, delete destination again")
+			print("file still in use, delete destination, try next time")
 			os.remove(dest)
 			return False
 			
